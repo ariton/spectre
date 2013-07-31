39d2ac07754a99c18b54363266f560819e1edf22
ariton <ariton@mzsoftware.de>
2013-07-31 10:26:30 +0200

    code  format

--- src/main/java/de/mzsoftware/spectre/GenericProxyFactory.java
+++ src/main/java/de/mzsoftware/spectre/GenericProxyFactory.java
@@ -13,7 +13,6 @@ import java.lang.reflect.Proxy;
 public class GenericProxyFactory {
 
     public static <T> T getProxy(Class<T> iface, final ClassLoader classLoader ) {
-        return (T)
-                Proxy.newProxyInstance(classLoader, new Class[]{iface}, new GenericProxyInvocationHandler());
+        return (T) Proxy.newProxyInstance(classLoader, new Class[]{iface}, new GenericProxyInvocationHandler());
     }
 }
